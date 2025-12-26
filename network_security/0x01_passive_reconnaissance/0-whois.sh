5892006161fd1dc95fbce1daca02c49cbaa0c4d6
lmova44-hub <lm.ova44@gmail.com>
2025-12-26 04:17:41 -0500

    task0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-whois "$1" | awk -F ': +' 'BEGIN{OFS=","}/^Registrant |^Admin |^Tech /{print $1,$2}' > "${1}.csv"
+whois "$1" | awk -F': +' '
+/^(Registrant|Admin|Tech)/ {
+    field=$1
+    value=$2
+    if(field ~ /Street$/) value=" "value" "
+    if(field ~ /Phone Ext|Fax Ext/ && value=="") value=""
+    printf "%s, %s", field, value
+    if(NR != 0) print ""
+}' > "$1.csv"
