788ba1d7c73679ffc14907cf37bde0a017dcb3cb
lmova44-hub <lm.ova44@gmail.com>
2025-12-26 04:22:43 -0500

    task991

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-whois "$1" | awk -F': +' '
-/^(Registrant|Admin|Tech)/ {
-    gsub(/^ +| +$/, "", $2)         # value-dən baş və son boşluqları sil
-    field=$1
-    value=$2
-    if(field ~ /Street$/) value=" "value" "
-    if(field ~ /Phone Ext|Fax Ext/ && value=="") value=""
-    print field", "value
-}' > "$1.csv"
+whois $1 | awk -F":" '/^(Registrant|Admin|Tech)/ {print $1 "," $2}' > $1.csv
