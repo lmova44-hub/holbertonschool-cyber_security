bca69e60e20ab6223a051aa398f75c151d925fbc
lmova44-hub <lm.ova44@gmail.com>
2026-02-09 07:06:33 -0500

    test

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 grep -E '^(Include|PasswordAuthentication)' /etc/ssh/sshd_config
+
+for f in /etc/ssh/sshd_config.d/*.conf 2>/dev/null; do
+    grep -E '^(Include|PasswordAuthentication)' "$f"
+done
