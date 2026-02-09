1fd14cadb70e13bc0647b82da6c5a27b383ebe1d
lmova44-hub <lm.ova44@gmail.com>
2026-02-09 07:05:03 -0500

    test

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-CONFIG_FILE="/etc/ssh/sshd_config"
-grep -Eh '^\s*[^#]' $CONFIG_FILE /etc/ssh/sshd_config.d/*.conf 2>/dev/null
+grep -E '^(Include|PasswordAuthentication)' /etc/ssh/sshd_config
