2d1aaf1b5b28aa6b51114d5367902710ed93d2ae
lmova44-hub <lm.ova44@gmail.com>
2026-04-08 02:02:17 -0400

    task0

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-LOG_FILE="logs.txt"
-
-if [ ! -f "$LOG_FILE" ]; then
-    echo "Error: $LOG_FILE not found."
-    exit 1
-fi
-
-awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -rn | awk '{print $2}' | head -1
+cat logs.txt | awk '{print $1}' | sort | uniq -c | sort -rn | head -1 | awk '{print $2}'
