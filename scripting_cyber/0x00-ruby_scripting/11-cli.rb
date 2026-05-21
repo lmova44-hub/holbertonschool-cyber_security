bff6b941ffcb4dcb8e477c7cfcdad8ff2b5aa491
lmova44-hub <lm.ova44@gmail.com>
2026-05-21 10:08:53 -0400

    Creating a Basic CLI Application

--- scripting_cyber/0x00-ruby_scripting/11-cli.rb
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -1,31 +1,28 @@
 #!/usr/bin/env ruby
 
-require 'fileutils'
 require 'optparse'
 
-TASKS_FILE = 'tasks.txt'.freeze
-
-FileUtils.touch(TASKS_FILE) unless File.exist?(TASKS_FILE)
+TASKS_FILE = 'tasks.txt'
 
 options = {}
 
-OptionParser.new do |option|
-  option.banner = 'Usage: cli.rb [options]'
+OptionParser.new do |opts|
+  opts.banner = "Usage: cli.rb [options]"
 
-  option.on('-a', '--add TASK', 'Add a new task') do |task|
+  opts.on("-a", "--add TASK", "Add a new task") do |task|
     options[:add] = task
   end
 
-  option.on('-l', '--list', 'List all tasks') do
+  opts.on("-l", "--list", "List all tasks") do
     options[:list] = true
   end
 
-  option.on('-r', '--remove INDEX', Integer, 'Remove a task by index') do |index|
-    options[:remove] = index
+  opts.on("-r", "--remove INDEX", "Remove a task by index") do |index|
+    options[:remove] = index.to_i
   end
 
-  option.on('-h', '--help', 'Show help') do
-    puts option
+  opts.on("-h", "--help", "Show help") do
+    puts opts
     exit
   end
 end.parse!
@@ -34,28 +31,26 @@ if options[:add]
   File.open(TASKS_FILE, 'a') do |file|
     file.puts(options[:add])
   end
+
   puts "Task '#{options[:add]}' added."
 
 elsif options[:list]
-  tasks = File.readlines(TASKS_FILE, chomp: true)
-  if tasks.empty?
-    puts 'No tasks found.'
-  else
+  if File.exist?(TASKS_FILE)
+    tasks = File.readlines(TASKS_FILE, chomp: true)
+
     tasks.each_with_index do |task, index|
       puts "#{index + 1}. #{task}"
     end
   end
 
 elsif options[:remove]
-  index = options[:remove].to_i
-  tasks = File.readlines(TASKS_FILE, chomp: true)
+  if File.exist?(TASKS_FILE)
+    tasks = File.readlines(TASKS_FILE, chomp: true)
 
-  if index.between?(1, tasks.size)
-    removed = tasks.delete_at(index - 1)
+    removed_task = tasks.delete_at(options[:remove] - 1)
+
     File.write(TASKS_FILE, tasks.join("\n") + "\n")
 
-    puts "Task '#{removed}' removed."
-  else
-    puts 'Invalid index.'
+    puts "Task '#{removed_task}' removed."
   end
 end
